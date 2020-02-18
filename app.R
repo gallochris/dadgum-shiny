54c80c37dbd19f235c2d7484353a67f829542851
Chris Gallo <christopher.joseph.gallo@gmail.com>
2020-02-18 06:39:00 -0500

    notre dame

--- app.R
+++ app.R
@@ -490,10 +490,11 @@ selectInput("opponentdef",
                                      "Game 22: Florida State" = 401168498,
                                      "Game 23: Duke" = 401168364,
                                      "Game 24: Wake Forest" = 401168499,
-                                     "Game 25: UVA" = 401168485
+                                     "Game 25: UVA" = 401168485,
+                                     "Game 26: Notre Dame" = 401168500
                                      
                          ),
-                         selected = c(401168485)
+                         selected = c(401168500)
       ),
      
       #Radio button input for presenting shot chart as heatmap
